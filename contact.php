a013753c527f9b0d5a5be20ced7834ac8d9bf500
ivawel3 <ivan@wellerindependent.com>
2018-01-27 15:45:22 -0800

    Removed unneccessary PHP version info from contact.php

--- contact.php
+++ contact.php
@@ -25,8 +25,7 @@
 
   // create email headers
   $headers = 'From: '.$email_from."\r\n".
-  'Reply-To: '.$email_from."\r\n" .
-  'X-Mailer: PHP/' . phpversion();
+  'Reply-To: '.$email_from."\r\n";
   mail($email_to, $email_subject, $email_message, $headers);
 
 ?>
