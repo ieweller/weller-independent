9f87a0612017403cc0e928d0580c10adce834946
wellindep-ivan <33075033+wellindep-ivan@users.noreply.github.com>
2018-01-27 11:32:04 -0800

    Initial commit

---
+++ contact.php
@@ -0,0 +1,32 @@
+<?php
+
+  $email_to = "contact@wellerindependent.com";
+  $email_subject = "Contact Request (Weller Independent)";
+
+  $name = $_POST['name']; // required
+  $company = $_POST['company']; // required
+  $email_from = $_POST['email']; // required
+  $phone = $_POST['phone']; // not required
+  $comments = $_POST['details']; // required
+
+  $email_message = "Form details below.\n\n";
+
+
+  function clean_string($string) {
+    $bad = array("content-type","bcc:","to:","cc:","href");
+    return str_replace($bad,"",$string);
+  }
+
+  $email_message .= "Name: ".clean_string($name)."\n";
+  $email_message .= "Company: ".clean_string($company)."\n";
+  $email_message .= "Email: ".clean_string($email_from)."\n";
+  $email_message .= "Telephone: ".clean_string($phone)."\n";
+  $email_message .= "Project Details: ".clean_string($comments)."\n";
+
+  // create email headers
+  $headers = 'From: '.$email_from."\r\n".
+  'Reply-To: '.$email_from."\r\n" .
+  'X-Mailer: PHP/' . phpversion();
+  @mail($email_to, $email_subject, $email_message, $headers);
+
+?>
