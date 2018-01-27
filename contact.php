b7d3c8ee4c02c2297c199204cd0175b4c35a6862
ivawel3 <ivan@wellerindependent.com>
2018-01-27 14:34:47 -0800

    Removed @ from mail

--- contact.php
+++ contact.php
@@ -27,6 +27,6 @@
   $headers = 'From: '.$email_from."\r\n".
   'Reply-To: '.$email_from."\r\n" .
   'X-Mailer: PHP/' . phpversion();
-  @mail($email_to, $email_subject, $email_message, $headers);
+  mail($email_to, $email_subject, $email_message, $headers);
 
 ?>
